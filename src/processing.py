9828f7c20610ddd2abd9c643fc208091e45aec02
Tobias Ritter <tobias.ritter98@gmail.com>
2024-04-14 23:16:58 +0200

    update get_data_loader

--- src/processing.py
+++ src/processing.py
@@ -3,6 +3,7 @@
 from src.abstract.abstract_data_loader import AbstractDataLoader
 from src.config import Config
 from src.data.abalone_data import AbaloneDataLoader
+from src.data.appliances_data import AppliancesDataLoader
 from src.data.energy_efficiency_data import EnergyEfficiencyDataLoader
 from src.data.mpg_data import MpgDataLoader
 from src.data.online_news_data import NewsDataLoader
@@ -20,5 +21,8 @@ def get_data_loader(config: Config) -> AbstractDataLoader:
 
     if config.data_set == "news":
         return NewsDataLoader(config)
+
+    if config.data_set == "appliances":
+        return AppliancesDataLoader(config)
 
     raise ValueError(f"Invalid data set: {config.data_set}")
